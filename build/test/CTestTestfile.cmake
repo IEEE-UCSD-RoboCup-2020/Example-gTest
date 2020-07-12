3c1bcdafc7867481ae104bc9345231cfe73bf373
Haoen Luo <samuelluohaoen@hotmail.com>
2020-07-11 19:35:18 -0700

    Removed build information

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/Code/Example-gTest/test
-# Build directory: /root/Code/Example-gTest/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(HelloWorld_Test "/root/Code/Example-gTest/build/test/HelloWorld_Test")
